eafcb94ebea1b8e7612bedc3fefdcd754774ba1b
sebasgonvitec <a01029746@itesm.mx>
2022-11-12 12:13:03 -0600

    Added RoombaModel

--- model.py
+++ model.py
@@ -11,4 +11,29 @@ class RoombaModel(Model):
         N: Number of Roomba agents
         height, width: Size of the grid
     """
-    
+    def __init__(self, N, width, height):
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, False)
+        self.schedule = RandomActivation(self)
+        self.running = True
+
+        # Create borders for the grid
+        border = [(x,y) for y in range(height) for x in range(width) if y in [0, height-1] or x in [0, width - 1]]
+        
+        for pos in border:
+            obs = ObstacleAgent(pos, self)
+            # self.schedule.add(obs)
+            self.grid.place_agent(obs, pos)
+
+        # Add the agent to the starting cell
+        for i in range(self.num_agents):
+            a = Roomba(i, self)
+            self.schedule.add(a)
+            self.grid.place_agent(a, (1,1))
+
+        # Add the trash to a random empty grid cell
+
+        def step(self):
+            '''Advance the model by one step.'''
+            self.schedule.step()
+            self.datacollector.collect(self)
